d14ae105a164a9814b08e4837c863ec101b57464
Frances Moncatar <pauline.moncatar@gmail.com>
2024-04-01 17:31:21 +0800

    BMI Calculator

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.bmicalculator;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.bmicalculator.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editTextWeight = findViewById(R.id.weight);
+        EditText editTextHeight = findViewById((R.id.height);
+        Button button = findViewById(R.id.btnSubmit);
+        TextView textView = findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float height = Float.parseFloat(String.valueOf(editTextHeight.getText())) / 100;
+                float weight = Float.parseFloat(String.valueOf(editTextWeight.getText()));
+                float bmi = weight / (height * height);
+                textView.setText(String.valueOf(bmi));
+            }
+        });
+    }
+}
